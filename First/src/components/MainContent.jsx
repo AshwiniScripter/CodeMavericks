06fa2e96707f6341ba467abd8d678a422200c758
Ashwini Salunke <162668451+AshwiniScripter@users.noreply.github.com>
2025-01-30 16:02:25 +0530

    initial commit

---
+++ First/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+        <div className='text-5xl text-center font-semibold mt-[70px]'>
+            <h1>Our College, Our<br></br>Community In <span className='text-blue-800'>Sync</span></h1>
+        </div>
+    </div>
+  )
+}
+
+export default MainContent
